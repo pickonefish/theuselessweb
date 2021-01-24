8d7f99e28fced249dc4427acbedac05867ba29e6
pickonefish <pickonefish@gmail.com>
2021-01-24 19:37:08 +0800

    feat: add shell fo docker build

---
+++ docker-build.sh
@@ -0,0 +1,20 @@
+cd drawing.garden
+sudo docker build -t pickonefish/drawing.garden:v1.0 .
+cd ..
+cd hackertyper
+sudo docker build -t pickonefish/hackertyper:v1.0 .
+cd ..
+cd jacksonpollock
+sudo docker build -t pickonefish/jacksonpollock:v1.0 .
+cd ..
+cd randomcolour
+sudo docker build -t pickonefish/randomcolour:v1.0 .
+cd ..
+cd republiquedesmangues.fr
+sudo docker build -t pickonefish/republiquedesmangues.fr:v1.0 .
+cd ..
+cd scroll-o-meter.club
+sudo docker build -t pickonefish/scroll-o-meter.club:v1.0 .
+cd ..
+cd smashthewalls
+sudo docker build -t pickonefish/smashthewalls:v1.0 .
